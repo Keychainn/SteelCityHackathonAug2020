334fae1ae5eb080492d00d2e873835570ecd216e
Keychainn <sunbakedcow@gmail.com>
2020-08-23 12:00:59 -0400

    finish code, no backend-frontend integration

--- src/app.py
+++ src/app.py
@@ -1,21 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from markupsafe import escape
+from generateLocations import getPOIList
+num=1
 # from flask_cors import CORS as cors
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/') # localhost:5000/
-def getdata():
-    geocode = 12.35
-    asdf  =   "hello there"
-    return render_template('tester.html', owovar=geocode, asdf=asdf)
+def returnIndex2():
+    return render_template('index.html')
 
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
+@app.route('/index/')
+def returnIndex():
+    return render_template('index.html')
+
+@app.route('/howToUse/')
+def returnHow():
+    return render_template('howToUse.html')
+
+@app.route('/tracker/')
+def returnTracker():
+    return render_template('tracker.html')
 
 
 
 
-if __name__=="main":
+if __name__=="__main__":
     app.run(debug=True) # for local use only. see deployment flask for hosting
